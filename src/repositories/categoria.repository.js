7c9bf980b965e19f8f57790bdcd6bc5533d0317b
ISABELLA DE SOUZA FLORENTINO <isabella.florentino@senaisp.edu.br>
2026-06-18 10:56:41 -0300

    ajustando bd

--- src/repositories/categoria.repository.js
+++ src/repositories/categoria.repository.js
@@ -3,31 +3,31 @@ import { connection } from "../config/Database.js";
 const categoriaModel = {
     cadastrar: async (dados) => {
         const sql = 'insert into categoria (descricaoCategoria, dataCad) values (?, NOW());';
-        const [rows] = await conn.execute(sql, [dados.descricaoCategoria]);
+        const [rows] = await connection.execute(sql, [dados.descricaoCategoria]);
         return rows;
     },
 
     listar: async () => {
         const sql = 'Select * from categoria;';
-        const [rows] = await conn.execute(sql);
+        const [rows] = await connection.execute(sql);
         return rows;
     },
 
     buscarPorId: async (id) => {
         const sql = 'select * from categoria where idCategoria = ?;';
-        const [rows] = await conn.execute(sql, [id]);
+        const [rows] = await connection.execute(sql, [id]);
         return rows[0];
     },
 
     editar: async (id, descricao) => {
         const sql = 'update categoria set descricaoCategoria = ? where idCategoria = ?;';
-        const [rows] = await conn.execute(sql, [descricao, id]);
+        const [rows] = await connection.execute(sql, [descricao, id]);
         return rows;
     },
 
     excluir: async (id) => {
         const sql = 'delete from categoria where idCategoria = ?;';
-        const [rows] = await conn.execute(sql, [id]);
+        const [rows] = await connection.execute(sql, [id]);
         return rows;
     }
 };
--- src/repositories/produtosrepository.js
+++ src/repositories/produtosrepository.js
@@ -5,32 +5,32 @@ const produtoModel = {
     cadastrar: async (p) => {
         const sql = 'insert into produtos (idCategoria, nomeProduto, valorProduto, vinculoImagem, dataCad) values (?, ?, ?, ?, NOW());';
         const values = [p.idCategoria, p.nomeProduto, p.valorProduto, p.vinculoImagem];
-        const [rows] = await conn.execute(sql, values);
+        const [rows] = await connection.execute(sql, values);
         return rows;
     },
 
     listarTodos: async () => {
         const sql = `Select p.*, c.descricaoCategoria from produtos p  inner join  categoria c on p.idCategoria = c.idCategoria;`;
-        const [rows] = await conn.execute(sql);
+        const [rows] = await connection.execute(sql);
         return rows;
     },
 
     listarId: async (id) => {
         const sql = `select p.*, c.descricaoCategoria  from produtos p inner join  categoria c ON p.idCategoria = c.idCategoria where p.idProduto = ?;`;
-        const [rows] = await conn.execute(sql, [id]);
+        const [rows] = await connection.execute(sql, [id]);
         return rows[0];
     },
 
     atualizar: async (id, p) => {
         const sql = 'update produtos set idCategoria=?, nomeProduto=?, valorProduto=?, vinculoImagem=? where idProduto=?;';
         const values = [p.idCategoria, p.nomeProduto, p.valorProduto, p.vinculoImagem, id];
-        const [rows] = await conn.execute(sql, values);
+        const [rows] = await connection.execute(sql, values);
         return rows;
     },
 
     excluir: async (id) => {
         const sql = 'delete from produtos where idProduto = ?;';
-        const [rows] = await conn.execute(sql, [id]);
+        const [rows] = await connection.execute(sql, [id]);
         return rows;
     }
 };
